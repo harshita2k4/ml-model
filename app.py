3a8858b6c9881f2789cd20c60b6ca6fb01a55e17
Harshita Sharma <harshita.shbt21@gmail.com>
2026-01-30 12:40:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
-import streamlit as streamlit
-import pandas as pd
-import pickle
-import numpy as np
-import os
+# --- PAGE CONFIG ---
+st.set_page_config(page_title="Iris Classifier", page_icon="🌸")
 
-st.set_page_config(page_title="Iris Classifier", page_icon="🪻")
-
+# --- LOAD THE TRAINED MODEL ---
 @st.cache_resource
 def load_model():
-
-    model_path="iris_model.pkl"
+    # Update the path to look for the .pkl file
+    model_path = "iris_model.pkl" 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as file:
             return pickle.load(file)
     else:
-        st.error(f"Model file '{model_path} not found!")
-        return none
-
+        st.error(f"Model file '{model_path}' not found!")
+        return None
     
 
     model = load_model()
